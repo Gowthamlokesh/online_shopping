9898fbf3f11a988de8af8000ba55035674c08bc9
Gowthamlokesh <30321320+Gowthamlokesh@users.noreply.github.com>
2017-12-05 19:10:17 +0530

    My Project Setup Completed Successfully

---
+++ shoppingbackend/src/main/java/lok/gow/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package lok.gow.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
